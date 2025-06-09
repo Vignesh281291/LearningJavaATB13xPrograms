ca5177e8dea57529db13f5309fb8a0dde85b4c42
Vignesh281291 <vignesh281291@gmail.com>
2025-06-10 00:37:35 +0530

    Lab003

--- src/ex_01_Java_Basic/Lab003_ShotcutHW.java
+++ src/ex_01_Java_Basic/Lab003_ShotcutHW.java
@@ -1,9 +1,10 @@
 package ex_01_Java_Basic;
 
 public class Lab003_ShotcutHW {
+    //SC-main
     public static void main(String[] args) {
 //        Author: Vignesh
-        System.out.println("Hi");
+        System.out.println("Hi"); // SC-sout
         System.out.println("Bye");
     }
 }
