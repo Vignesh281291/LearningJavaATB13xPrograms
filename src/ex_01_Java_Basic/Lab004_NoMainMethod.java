eab88222a79d6a873fa2142d97cb52ca61d1f865
Vignesh281291 <vignesh281291@gmail.com>
2025-06-10 01:07:12 +0530

    Lab003

--- src/ex_01_Java_Basic/Lab004_NoMainMethod.java
+++ src/ex_01_Java_Basic/Lab004_NoMainMethod.java
@@ -2,6 +2,9 @@ package ex_01_Java_Basic;
 
 public class Lab004_NoMainMethod {
 //without main ur code cannot be executed
+public static void main(String[] args) {
+
+}
     }
 //main method should be inside class
 //public static void main(String[] args) {
