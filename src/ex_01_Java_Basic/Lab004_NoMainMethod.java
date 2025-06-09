52cc36090487b4f80fc18fc201f7bd44f5f886fe
Vignesh281291 <vignesh281291@gmail.com>
2025-06-10 00:38:18 +0530

    Lab003

---
+++ src/ex_01_Java_Basic/Lab004_NoMainMethod.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab004_NoMainMethod {
+//without main ur code cannot be executed
+    }
+//main method should be inside class
+//public static void main(String[] args) {
---
+++ src/ex_01_Java_Basic/Lab005_Commnets.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basic;
+
+public class Lab005_Commnets {
+    public static void main(String[] args) {
+        //single line comment
+        /*multiple line comments
+        line2
+        line3
+        lin4
+         */
+        /*System.out.println("1");
+        System.out.println("2");*/
+    }
+}
