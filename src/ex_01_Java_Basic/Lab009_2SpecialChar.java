2fbf878bc36f1caab54a5bf217eec2f53676d4aa
Vignesh281291 <vignesh281291@gmail.com>
2025-06-10 01:28:00 +0530

    Lab009

---
+++ src/ex_01_Java_Basic/Lab009_2SpecialChar.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab009_2SpecialChar {
+    public static void main(String[] args) {
+        System.out.println("$ and _ can only be the special char used in the class name");
+    }
+}
