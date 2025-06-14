0f4b11de86ef9d1fcc169fa0f6323ab765c3fe43
Vignesh281291 <vignesh281291@gmail.com>
2025-06-15 00:33:53 +0530

    TaskDay

---
+++ src/ex_05_Operators/Lab051_InterviewQ.java
@@ -0,0 +1,13 @@
+package ex_05_Operators;
+
+public class Lab051_InterviewQ {
+    public static void main(String[] args) {
+        char a ='A';
+        String s = "10";
+        //String s = "A"
+        short s1=5;
+        System.out.println(a+s1);
+        System.out.println(a+s); //Basically char is an integer
+        // Only when the char is with its int datatype, the char acts as a int
+    }
+}
