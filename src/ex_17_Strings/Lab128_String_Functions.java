c9d31b0229602c53ba884fc1d540c04bd28eb12b
Vignesh281291 <vignesh281291@gmail.com>
2025-06-24 00:28:48 +0530

    Exercises23rdJune

---
+++ src/ex_17_Strings/Lab128_String_Functions.java
@@ -0,0 +1,11 @@
+package ex_17_Strings;
+
+public class Lab128_String_Functions {
+    public static void main(String[] args) {
+        String s = "Vicky";
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+        System.out.println(s.concat("Vihaana"));
+        System.out.println(s.length());
+    }
+}
