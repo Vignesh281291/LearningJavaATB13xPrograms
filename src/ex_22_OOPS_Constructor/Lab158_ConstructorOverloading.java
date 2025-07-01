529ae7ebb4cda2bd633888466e5b70e0a45486da
Vignesh281291 <vignesh281291@gmail.com>
2025-07-01 14:13:57 +0530

    Day20Constructor

---
+++ src/ex_22_OOPS_Constructor/Lab158_ConstructorOverloading.java
@@ -0,0 +1,47 @@
+package ex_22_OOPS_Constructor;
+
+public class Lab158_ConstructorOverloading {
+    public static void main(String[] args) {
+        PersonA p1 = new PersonA("Vignesh", 33, 100);
+        System.out.println(p1.name);
+        System.out.println(p1.age);
+        System.out.println(p1.salary);
+        PersonA p2 = new PersonA("SE", 28);
+        System.out.println(p2.occupation);
+        System.out.println(p2.dob);
+        PersonA p3 = new PersonA();
+        p3.module = "Test2 Module";
+        System.out.println(p3.module);
+        System.out.println(p3.join_date);
+    }
+}
+class PersonA
+{
+    String name;
+    int age;
+    int salary;
+    String occupation;
+    int dob;
+    String module;
+    int join_date;
+    //Constructor with parameter
+    PersonA(String name, int age, int salary)
+    {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+    //Constructor with parameters - Constructor Overloading
+    PersonA(String occupation, int dob)
+    {
+        this.occupation = occupation;
+        this.dob = dob;
+    }
+
+    //Default Constructor
+    PersonA()
+    {
+        module = "Test1 Module";
+        join_date = 30;
+    }
+}
