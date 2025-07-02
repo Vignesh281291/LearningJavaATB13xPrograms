85293389f27d95ec61ad838133fb5ee90f4f0c35
Vignesh281291 <vignesh281291@gmail.com>
2025-07-03 01:10:10 +0530

    Day21-Inheritance

--- src/ex_19_Arrays/Lab139_Array_sortandprint.java
+++ src/ex_19_Arrays/Lab139_Array_sortandprint.java
@@ -7,7 +7,7 @@ public class Lab139_Array_sortandprint {
         int[] marks = {15,5,10,2}; // length -> 4 Index -> 3
         //printing the values using loop
         //sorting using function
-        Arrays.sort(marks);
+        //Arrays.sort(marks);
         for (int i = 0; i < marks.length; i++) {
             System.out.println(marks[i]);
         }
---
+++ src/ex_23_Inheritance/Lab159_Single_Inheritance.java
@@ -0,0 +1,29 @@
+package ex_23_Inheritance;
+
+public class Lab159_Single_Inheritance {
+    public static void main(String[] args) {
+    B o1=new B();
+    o1.func1();
+    A o2 = new A();
+    // o2.func2(); Cannot call func2
+    o1.func2();
+    }
+}
+    class A
+    {
+        int a = 300;
+        void func1()
+        {
+            String s = "abc";
+            int i = 35;
+            System.out.println("Parent:" + s + i + a);
+        }
+    }
+    class B extends A
+    {
+        int b=500;
+        void func2()
+        {
+            System.out.println("child:" + b);
+            }
+        }
